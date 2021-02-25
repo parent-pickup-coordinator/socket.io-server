a309f6ff02ddb24073663c61eb808377ec4fadd9
Simon Panek <paneks19@gmail.com>
2021-02-25 15:10:14 -0800

    comment out io.set

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const io = socketIO(httpServer, {
   }
 });
 
-io.set('transports', ['websocket']); //from https://stackoverflow.com/questions/25268160/node-js-socket-io-on-heroku-throws-h13
+// io.set('transports', ['websocket']); //from https://stackoverflow.com/questions/25268160/node-js-socket-io-on-heroku-throws-h13
 
 io.on('connection', (socket) => {
 
